b84f9446d58f3b1d1ab729c4b422530817c0969d
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2025-04-08 14:47:37 +0000

    config: add force-local and operations-center host
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- cmd/operations-center/internal/config/config.go
+++ cmd/operations-center/internal/config/config.go
@@ -1,5 +1,31 @@
 package config
 
+import (
+	"errors"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+)
+
 type Config struct {
-	OperationsCenterServer string
+	// Config from global flags
+	Verbose    bool `yaml:"-"`
+	Debug      bool `yaml:"-"`
+	ForceLocal bool `yaml:"-"`
+
+	OperationsCenterServer string `yaml:"operations_center_server"`
+}
+
+func (c *Config) LoadConfig(path string) error {
+	contents, err := os.ReadFile(filepath.Join(path, "config.yml"))
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+
+		return err
+	}
+
+	return yaml.Unmarshal(contents, c)
 }
