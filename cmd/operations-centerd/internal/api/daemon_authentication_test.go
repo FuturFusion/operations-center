15d7fa0e51641cfb58e2ffd1fc0fd92b1015a175
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2025-04-16 15:03:09 +0000

    operations-centerd: Add tests with openfga
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- cmd/operations-centerd/internal/api/daemon_authentication_test.go
+++ cmd/operations-centerd/internal/api/daemon_authentication_test.go
@@ -1,23 +1,45 @@
 package api_test
 
 import (
+	"bytes"
 	"context"
 	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"io"
 	"net"
 	"net/http"
 	"path/filepath"
 	"testing"
 	"time"
 
+	"github.com/docker/go-connections/nat"
 	incustls "github.com/lxc/incus/v6/shared/tls"
 	"github.com/oauth2-proxy/mockoidc"
 	"github.com/stretchr/testify/require"
+	testcontainers "github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/openfga"
 
 	"github.com/FuturFusion/operations-center/cmd/operations-centerd/internal/api"
 	"github.com/FuturFusion/operations-center/cmd/operations-centerd/internal/config"
 )
 
+const oidcCode = `123`
+
+const (
+	admin    = "admin"
+	operator = "operator"
+	viewer   = "viewer"
+)
+
+var users = []string{
+	admin,
+	operator,
+	viewer,
+}
+
 func TestAuthentication(t *testing.T) {
+	ctx := context.Background()
 	tmpDir := t.TempDir()
 
 	// Setup client certificate
@@ -29,38 +51,23 @@ func TestAuthentication(t *testing.T) {
 
 	certFingerprint := incustls.CertFingerprint(cert.Leaf)
 
-	// Setup OIDC, get accessToken
-	m, err := mockoidc.Run()
-	require.NoError(t, err)
-	t.Cleanup(func() {
-		err = m.Shutdown()
-		require.NoError(t, err)
-	})
+	oidcProvider, accessTokens := setupMockOIDC(t)
 
-	m.QueueCode("123")
-	sess, err := m.SessionStore.NewSession("openid email profile groups", "", mockoidc.DefaultUser(), "123", "")
-	require.NoError(t, err)
-
-	accessToken, err := sess.AccessToken(&mockoidc.Config{
-		ClientID:     m.Config().ClientID,
-		ClientSecret: m.Config().ClientSecret,
-		Issuer:       m.Issuer(),
-		AccessTTL:    5 * time.Second,
-		RefreshTTL:   5 * time.Second,
-	}, m.Keypair, time.Now())
-	require.NoError(t, err)
+	openFGAEndpoint, openFGAStoreID := setupOpenFGA(ctx, t)
 
 	// Test cases
 	tests := []struct {
 		name     string
 		client   func() *http.Client
+		method   string
 		resource string
 		headers  map[string]string
+		body     io.Reader
 
 		wantStatusCode int
 	}{
 		{
-			name: "plain http /",
+			name: "plain http GET /",
 			client: func() *http.Client {
 				return &http.Client{
 					Transport: &http.Transport{
@@ -70,12 +77,14 @@ func TestAuthentication(t *testing.T) {
 					},
 				}
 			},
+			method:   http.MethodGet,
 			resource: "https://localhost:17443/",
+			body:     http.NoBody,
 
 			wantStatusCode: http.StatusOK,
 		},
 		{
-			name: "socket /",
+			name: "socket GET /",
 			client: func() *http.Client {
 				return &http.Client{
 					Transport: &http.Transport{
@@ -85,12 +94,14 @@ func TestAuthentication(t *testing.T) {
 					},
 				}
 			},
+			method:   http.MethodGet,
 			resource: "http://unix.socket/",
+			body:     http.NoBody,
 
 			wantStatusCode: http.StatusOK,
 		},
 		{
-			name: "plain http /1.0 - forbidden",
+			name: "plain http GET /1.0 - forbidden",
 			client: func() *http.Client {
 				return &http.Client{
 					Transport: &http.Transport{
@@ -100,12 +111,14 @@ func TestAuthentication(t *testing.T) {
 					},
 				}
 			},
+			method:   http.MethodGet,
 			resource: "https://localhost:17443/1.0",
+			body:     http.NoBody,
 
 			wantStatusCode: http.StatusForbidden,
 		},
 		{
-			name: "socket /1.0",
+			name: "socket GET /1.0",
 			client: func() *http.Client {
 				return &http.Client{
 					Transport: &http.Transport{
@@ -115,12 +128,14 @@ func TestAuthentication(t *testing.T) {
 					},
 				}
 			},
+			method:   http.MethodGet,
 			resource: "http://unix.socket/1.0",
+			body:     http.NoBody,
 
 			wantStatusCode: http.StatusOK,
 		},
 		{
-			name: "client cert http /1.0",
+			name: "client cert http GET /1.0",
 			client: func() *http.Client {
 				return &http.Client{
 					Transport: &http.Transport{
@@ -131,12 +146,14 @@ func TestAuthentication(t *testing.T) {
 					},
 				}
 			},
+			method:   http.MethodGet,
 			resource: "https://localhost:17443/1.0",
+			body:     http.NoBody,
 
 			wantStatusCode: http.StatusOK,
 		},
 		{
-			name: "oidc http /1.0",
+			name: "oidc http GET /1.0 as viewer",
 			client: func() *http.Client {
 				return &http.Client{
 					Transport: &http.Transport{
@@ -147,16 +164,289 @@ func TestAuthentication(t *testing.T) {
 					},
 				}
 			},
+			method:   http.MethodGet,
 			resource: "https://localhost:17443/1.0",
 			headers: map[string]string{
-				"Authorization": "Bearer " + accessToken,
+				"Authorization": "Bearer " + accessTokens[viewer],
 			},
+			body: http.NoBody,
+
+			wantStatusCode: http.StatusOK,
+		},
+		{
+			name: "oidc http GET /1.0/provisioning/servers as viewer",
+			client: func() *http.Client {
+				return &http.Client{
+					Transport: &http.Transport{
+						TLSClientConfig: &tls.Config{
+							Certificates:       []tls.Certificate{cert},
+							InsecureSkipVerify: true,
+						},
+					},
+				}
+			},
+			method:   http.MethodGet,
+			resource: "https://localhost:17443/1.0/provisioning/servers",
+			headers: map[string]string{
+				"Authorization": "Bearer " + accessTokens[viewer],
+			},
+			body: http.NoBody,
+
+			wantStatusCode: http.StatusOK,
+		},
+		{
+			name: "oidc http GET /1.0/provisioning/servers as admin",
+			client: func() *http.Client {
+				return &http.Client{
+					Transport: &http.Transport{
+						TLSClientConfig: &tls.Config{
+							Certificates:       []tls.Certificate{cert},
+							InsecureSkipVerify: true,
+						},
+					},
+				}
+			},
+			method:   http.MethodGet,
+			resource: "https://localhost:17443/1.0/provisioning/servers",
+			headers: map[string]string{
+				"Authorization": "Bearer " + accessTokens[admin],
+			},
+			body: http.NoBody,
+
+			wantStatusCode: http.StatusOK,
+		},
+		{
+			name: "oidc http GET /1.0/provisioning/servers as operator",
+			client: func() *http.Client {
+				return &http.Client{
+					Transport: &http.Transport{
+						TLSClientConfig: &tls.Config{
+							Certificates:       []tls.Certificate{cert},
+							InsecureSkipVerify: true,
+						},
+					},
+				}
+			},
+			method:   http.MethodGet,
+			resource: "https://localhost:17443/1.0/provisioning/servers",
+			headers: map[string]string{
+				"Authorization": "Bearer " + accessTokens[operator],
+			},
+			body: http.NoBody,
+
+			wantStatusCode: http.StatusOK,
+		},
+		{
+			name: "oidc http POST /1.0/provisioning/servers as viewer - forbidden",
+			client: func() *http.Client {
+				return &http.Client{
+					Transport: &http.Transport{
+						TLSClientConfig: &tls.Config{
+							Certificates:       []tls.Certificate{cert},
+							InsecureSkipVerify: true,
+						},
+					},
+				}
+			},
+			method:   http.MethodPost,
+			resource: "https://localhost:17443/1.0/provisioning/servers",
+			headers: map[string]string{
+				"Authorization": "Bearer " + accessTokens[viewer],
+			},
+			body: http.NoBody,
+
+			wantStatusCode: http.StatusForbidden,
+		},
+		{
+			name: "oidc http POST /1.0/provisioning/servers as operator - forbidden",
+			client: func() *http.Client {
+				return &http.Client{
+					Transport: &http.Transport{
+						TLSClientConfig: &tls.Config{
+							Certificates:       []tls.Certificate{cert},
+							InsecureSkipVerify: true,
+						},
+					},
+				}
+			},
+			method:   http.MethodPost,
+			resource: "https://localhost:17443/1.0/provisioning/servers",
+			headers: map[string]string{
+				"Authorization": "Bearer " + accessTokens[operator],
+			},
+			body: bytes.NewBufferString(`{
+  "name": "serverA",
+  "connection_url": "https://127.0.0.1:12345/",
+  "server_type": "incus"
+}`),
+
+			wantStatusCode: http.StatusForbidden,
+		},
+		{
+			name: "oidc http POST /1.0/provisioning/servers as admin",
+			client: func() *http.Client {
+				return &http.Client{
+					Transport: &http.Transport{
+						TLSClientConfig: &tls.Config{
+							Certificates:       []tls.Certificate{cert},
+							InsecureSkipVerify: true,
+						},
+					},
+				}
+			},
+			method:   http.MethodPost,
+			resource: "https://localhost:17443/1.0/provisioning/servers",
+			headers: map[string]string{
+				"Authorization": "Bearer " + accessTokens[admin],
+			},
+			body: bytes.NewBufferString(`{
+  "name": "serverA",
+  "connection_url": "https://127.0.0.1:12345/",
+  "server_type": "incus"
+}`),
+
+			wantStatusCode: http.StatusCreated,
+		},
+		{
+			name: "oidc http PUT /1.0/provisioning/servers/serverA as viewer",
+			client: func() *http.Client {
+				return &http.Client{
+					Transport: &http.Transport{
+						TLSClientConfig: &tls.Config{
+							Certificates:       []tls.Certificate{cert},
+							InsecureSkipVerify: true,
+						},
+					},
+				}
+			},
+			method:   http.MethodPut,
+			resource: "https://localhost:17443/1.0/provisioning/servers/serverA",
+			headers: map[string]string{
+				"Authorization": "Bearer " + accessTokens[viewer],
+			},
+			body: bytes.NewBufferString(`{
+  "name": "serverA",
+  "connection_url": "https://viewer:12346/",
+  "server_type": "incus"
+}`),
+
+			wantStatusCode: http.StatusForbidden,
+		},
+		{
+			name: "oidc http PUT /1.0/provisioning/servers/serverA as operator",
+			client: func() *http.Client {
+				return &http.Client{
+					Transport: &http.Transport{
+						TLSClientConfig: &tls.Config{
+							Certificates:       []tls.Certificate{cert},
+							InsecureSkipVerify: true,
+						},
+					},
+				}
+			},
+			method:   http.MethodPut,
+			resource: "https://localhost:17443/1.0/provisioning/servers/serverA",
+			headers: map[string]string{
+				"Authorization": "Bearer " + accessTokens[operator],
+			},
+			body: bytes.NewBufferString(`{
+  "name": "serverA",
+  "connection_url": "https://operator:12346/",
+  "server_type": "incus"
+}`),
+
+			wantStatusCode: http.StatusCreated,
+		},
+		{
+			name: "oidc http PUT /1.0/provisioning/servers/serverA as admin",
+			client: func() *http.Client {
+				return &http.Client{
+					Transport: &http.Transport{
+						TLSClientConfig: &tls.Config{
+							Certificates:       []tls.Certificate{cert},
+							InsecureSkipVerify: true,
+						},
+					},
+				}
+			},
+			method:   http.MethodPut,
+			resource: "https://localhost:17443/1.0/provisioning/servers/serverA",
+			headers: map[string]string{
+				"Authorization": "Bearer " + accessTokens[admin],
+			},
+			body: bytes.NewBufferString(`{
+  "name": "serverA",
+  "connection_url": "https://admin:12346/",
+  "server_type": "incus"
+}`),
+
+			wantStatusCode: http.StatusCreated,
+		},
+		{
+			name: "oidc http DELETE /1.0/provisioning/servers/serverA as viewer",
+			client: func() *http.Client {
+				return &http.Client{
+					Transport: &http.Transport{
+						TLSClientConfig: &tls.Config{
+							Certificates:       []tls.Certificate{cert},
+							InsecureSkipVerify: true,
+						},
+					},
+				}
+			},
+			method:   http.MethodDelete,
+			resource: "https://localhost:17443/1.0/provisioning/servers/serverA",
+			headers: map[string]string{
+				"Authorization": "Bearer " + accessTokens[viewer],
+			},
+			body: http.NoBody,
+
+			wantStatusCode: http.StatusForbidden,
+		},
+		{
+			name: "oidc http DELETE /1.0/provisioning/servers/serverA as operator",
+			client: func() *http.Client {
+				return &http.Client{
+					Transport: &http.Transport{
+						TLSClientConfig: &tls.Config{
+							Certificates:       []tls.Certificate{cert},
+							InsecureSkipVerify: true,
+						},
+					},
+				}
+			},
+			method:   http.MethodDelete,
+			resource: "https://localhost:17443/1.0/provisioning/servers/serverA",
+			headers: map[string]string{
+				"Authorization": "Bearer " + accessTokens[operator],
+			},
+			body: http.NoBody,
+
+			wantStatusCode: http.StatusForbidden,
+		},
+		{
+			name: "oidc http DELETE /1.0/provisioning/servers/serverA as admin",
+			client: func() *http.Client {
+				return &http.Client{
+					Transport: &http.Transport{
+						TLSClientConfig: &tls.Config{
+							Certificates:       []tls.Certificate{cert},
+							InsecureSkipVerify: true,
+						},
+					},
+				}
+			},
+			method:   http.MethodDelete,
+			resource: "https://localhost:17443/1.0/provisioning/servers/serverA",
+			headers: map[string]string{
+				"Authorization": "Bearer " + accessTokens[admin],
+			},
+			body: http.NoBody,
 
 			wantStatusCode: http.StatusOK,
 		},
 	}
 
-	ctx := context.Background()
 	d := api.NewDaemon(
 		ctx,
 		mockEnv{
@@ -165,10 +455,13 @@ func TestAuthentication(t *testing.T) {
 		},
 		&config.Config{
 			RestServerPort:                   17443,
-			OidcIssuer:                       m.Issuer(),
-			OidcClientID:                     m.ClientID,
+			OidcIssuer:                       oidcProvider.Issuer(),
+			OidcClientID:                     oidcProvider.ClientID,
 			OidcScope:                        "openid,offline_access,email",
 			TrustedTLSClientCertFingerprints: []string{certFingerprint},
+			OpenfgaAPIURL:                    openFGAEndpoint,
+			OpenfgaAPIToken:                  "dummy",
+			OpenfgaStoreID:                   openFGAStoreID,
 		},
 	)
 
@@ -179,9 +472,11 @@ func TestAuthentication(t *testing.T) {
 		require.NoError(t, err)
 	})
 
+	setupOpenFGATuples(t, openFGAEndpoint, openFGAStoreID)
+
 	for _, tc := range tests {
-		t.Run(tc.resource, func(t *testing.T) {
-			req, err := http.NewRequest(http.MethodGet, tc.resource, http.NoBody)
+		t.Run(tc.name, func(t *testing.T) {
+			req, err := http.NewRequest(tc.method, tc.resource, tc.body)
 			require.NoError(t, err)
 
 			for key, value := range tc.headers {
@@ -194,5 +489,99 @@ func TestAuthentication(t *testing.T) {
 
 			require.Equal(t, tc.wantStatusCode, resp.StatusCode)
 		})
+	}
+}
+
+func setupMockOIDC(t *testing.T) (*mockoidc.MockOIDC, map[string]string) {
+	t.Helper()
+
+	m, err := mockoidc.Run()
+	require.NoError(t, err)
+
+	t.Cleanup(func() {
+		err = m.Shutdown()
+		require.NoError(t, err)
+	})
+
+	accessTokens := make(map[string]string, len(users))
+
+	for _, user := range users {
+		m.QueueCode(oidcCode)
+		sess, err := m.SessionStore.NewSession("openid email profile groups", "", &mockoidc.MockUser{
+			Subject: user,
+		}, oidcCode, "")
+		require.NoError(t, err)
+
+		accessToken, err := sess.AccessToken(&mockoidc.Config{
+			ClientID:     m.Config().ClientID,
+			ClientSecret: m.Config().ClientSecret,
+			Issuer:       m.Issuer(),
+			AccessTTL:    1 * time.Minute,
+			RefreshTTL:   1 * time.Minute,
+		}, m.Keypair, time.Now())
+		require.NoError(t, err)
+
+		accessTokens[user] = accessToken
+	}
+
+	return m, accessTokens
+}
+
+func setupOpenFGA(ctx context.Context, t *testing.T) (endpoint string, storeID string) {
+	t.Helper()
+
+	openfgaContainer, err := openfga.Run(ctx, "openfga/openfga:v1.8.9")
+	require.NoError(t, err)
+
+	t.Cleanup(func() {
+		err = testcontainers.TerminateContainer(openfgaContainer)
+		require.NoError(t, err)
+	})
+
+	openFGAEndpoint, err := openfgaContainer.PortEndpoint(ctx, nat.Port("8080"), "")
+	require.NoError(t, err)
+	openFGAEndpoint = "http://" + openFGAEndpoint
+
+	resp, err := http.Post(openFGAEndpoint+"/stores", "application/json", bytes.NewBufferString(`{"name": "operations-center"}`))
+	require.NoError(t, err)
+	defer resp.Body.Close()
+
+	createStoreResponseBody, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
+
+	var createStoreResponse struct {
+		ID string `json:"id"`
+	}
+
+	err = json.Unmarshal(createStoreResponseBody, &createStoreResponse)
+	require.NoError(t, err)
+
+	return openFGAEndpoint, createStoreResponse.ID
+}
+
+func setupOpenFGATuples(t *testing.T, endpoint string, storeID string) {
+	t.Helper()
+
+	for _, user := range users {
+		resp, err := http.Post(
+			fmt.Sprintf("%s/stores/%s/write", endpoint, storeID),
+			"application/json",
+			bytes.NewBufferString(
+				fmt.Sprintf(`{
+  "writes": {
+    "tuple_keys": [
+      {
+        "user": "user:%[1]s",
+        "relation": "%[1]s",
+        "object": "server:operations-center"
+      }
+    ]
+  }
+}`, user),
+			),
+		)
+		require.NoError(t, err)
+		resp.Body.Close()
+		require.Equal(t, http.StatusOK, resp.StatusCode)
 	}
 }
--- go.mod
+++ go.mod
@@ -7,8 +7,10 @@ require golang.org/x/sys v0.31.0
 require (
 	cel.dev/expr v0.22.0 // indirect
 	dario.cat/mergo v1.0.1 // indirect
+	github.com/Azure/go-ansiterm v0.0.0-20230124172434-306776ec8161 // indirect
 	github.com/Masterminds/goutils v1.1.1 // indirect
 	github.com/Masterminds/semver/v3 v3.3.0 // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/Rican7/retry v0.3.1 // indirect
 	github.com/Yiling-J/theine-go v0.6.1 // indirect
 	github.com/antlr4-go/antlr/v4 v4.13.1 // indirect
@@ -18,12 +20,20 @@ require (
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/chigopher/pathlib v0.19.1 // indirect
+	github.com/containerd/log v0.1.0 // indirect
+	github.com/containerd/platforms v0.2.1 // indirect
 	github.com/cowsql/go-cowsql v1.22.0 // indirect
+	github.com/cpuguy83/dockercfg v0.3.2 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/distribution/reference v0.6.0 // indirect
+	github.com/docker/docker v28.0.1+incompatible // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/ebitengine/purego v0.8.2 // indirect
 	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/envoyproxy/protoc-gen-validate v1.2.1 // indirect
 	github.com/fatih/color v1.18.0 // indirect
 	github.com/fatih/structs v1.1.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/flosch/pongo2/v6 v6.0.0 // indirect
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/go-chi/chi/v5 v5.2.1 // indirect
@@ -31,8 +41,10 @@ require (
 	github.com/go-jose/go-jose/v4 v4.0.5 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/gocarina/gocsv v0.0.0-20240520201108-78e41c74b4b1 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
 	github.com/google/cel-go v0.24.1 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
@@ -49,6 +61,7 @@ require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jedib0t/go-pretty/v6 v6.6.7 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
 	github.com/knadh/koanf/maps v0.1.1 // indirect
 	github.com/knadh/koanf/parsers/yaml v0.1.0 // indirect
@@ -58,12 +71,21 @@ require (
 	github.com/knadh/koanf/providers/structs v0.1.0 // indirect
 	github.com/knadh/koanf/v2 v2.1.2 // indirect
 	github.com/kr/fs v0.1.0 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/magiconair/properties v1.8.9 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/moby/docker-image-spec v1.3.1 // indirect
+	github.com/moby/patternmatcher v0.6.0 // indirect
+	github.com/moby/sys/sequential v0.5.0 // indirect
+	github.com/moby/sys/user v0.1.0 // indirect
+	github.com/moby/sys/userns v0.1.0 // indirect
+	github.com/moby/term v0.5.0 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/muesli/mango v0.2.0 // indirect
 	github.com/muesli/mango-cobra v1.2.0 // indirect
 	github.com/muesli/mango-pflag v0.1.0 // indirect
@@ -74,6 +96,8 @@ require (
 	github.com/natefinch/wrap v0.2.0 // indirect
 	github.com/nwidger/jsoncolor v0.3.2 // indirect
 	github.com/oklog/ulid/v2 v2.1.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.1 // indirect
 	github.com/openfga/api/proto v0.0.0-20250127102726-f9709139a369 // indirect
 	github.com/openfga/language/pkg/go v0.2.0-beta.2.0.20250220223040-ed0cfba54336 // indirect
 	github.com/openfga/openfga v1.8.7 // indirect
@@ -82,6 +106,7 @@ require (
 	github.com/pkg/sftp v1.13.9 // indirect
 	github.com/pkg/xattr v0.4.10 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.63.0 // indirect
 	github.com/prometheus/procfs v0.16.0 // indirect
@@ -92,6 +117,7 @@ require (
 	github.com/rung/go-safecast v1.0.1 // indirect
 	github.com/sagikazarmark/locafero v0.9.0 // indirect
 	github.com/schollz/progressbar/v3 v3.18.0 // indirect
+	github.com/shirou/gopsutil/v4 v4.25.1 // indirect
 	github.com/shopspring/decimal v1.4.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
@@ -100,13 +126,17 @@ require (
 	github.com/spf13/viper v1.20.1 // indirect
 	github.com/stoewer/go-strcase v1.3.0 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	github.com/zeebo/xxh3 v1.0.2 // indirect
 	github.com/zitadel/logging v0.6.2 // indirect
 	github.com/zitadel/schema v1.3.1 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.60.0 // indirect
 	go.opentelemetry.io/otel v1.35.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.35.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.35.0 // indirect
