a52cffc3829c926e94b8e61e4cbae037e8d8ea7f
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2025-04-17 12:36:59 +0000

    authn: Refactor authenticators, process as a chain
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- internal/authn/authenticate_middleware.go
+++ internal/authn/authenticate_middleware.go
@@ -2,43 +2,56 @@ package authn
 
 import (
 	"context"
-	"fmt"
 	"log/slog"
 	"net/http"
 
-	"github.com/FuturFusion/operations-center/internal/authn/oidc"
-	"github.com/FuturFusion/operations-center/internal/authn/tls"
+	"github.com/FuturFusion/operations-center/internal/logger"
 	"github.com/FuturFusion/operations-center/internal/response"
-	"github.com/FuturFusion/operations-center/shared/api"
 )
 
+// Middleware returns a http handler middleware, which will try to authenticate
+// a request by probing all provided authers in sequence.
+// When successful, the authenticated username and the protocol of the
+// authentication is stored in the request context.
 func (a Authenticator) Middleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var username, protocol string
+		var trusted bool
+		var err error
+
+		ctx := r.Context()
+
 		// Authentication
-		trusted, username, protocol, err := a.authenticate(w, r)
-		if err != nil {
-			_, ok := err.(*oidc.AuthError)
-			if ok {
-				// Ensure the OIDC headers are set if needed.
-				if a.oidcVerifier != nil {
-					_ = a.oidcVerifier.WriteHeaders(w)
+		for _, auther := range a.authers {
+			trusted, username, protocol, err = auther.Auth(w, r)
+			if err != nil {
+				err = response.Unauthorized(err).Render(w)
+				if err != nil {
+					slog.WarnContext(ctx, "Render error response failed", logger.Err(err))
 				}
 
-				_ = response.Unauthorized(err).Render(w)
 				return
+			}
+
+			if trusted {
+				break
 			}
 		}
 
 		if !trusted {
-			slog.WarnContext(r.Context(), "Rejecting request from untrusted client", slog.String("ip", r.RemoteAddr), slog.String("path", r.RequestURI), slog.String("method", r.Method))
-			_ = response.Forbidden(nil).Render(w)
+			slog.WarnContext(ctx, "Rejecting request from untrusted client", slog.String("ip", r.RemoteAddr), slog.String("path", r.RequestURI), slog.String("method", r.Method))
+			err = response.Forbidden(nil).Render(w)
+			if err != nil {
+				slog.WarnContext(ctx, "Render forbidden response failed", logger.Err(err))
+			}
+
 			return
 		}
 
-		slog.DebugContext(r.Context(), "Handling API request", slog.String("method", r.Method), slog.String("url", r.URL.RequestURI()), slog.String("ip", r.RemoteAddr))
+		slog.DebugContext(ctx, "Handling API request", slog.String("method", r.Method), slog.String("url", r.URL.RequestURI()), slog.String("ip", r.RemoteAddr))
 
 		// Add authentication/authorization context data.
-		ctx := context.WithValue(r.Context(), CtxUsername, username)
+		ctx = context.WithValue(ctx, CtxUsername, username)
 		ctx = context.WithValue(ctx, CtxProtocol, protocol)
 
 		r = r.WithContext(ctx)
@@ -46,43 +59,4 @@ func (a Authenticator) Middleware(next http.HandlerFunc) http.HandlerFunc {
 		// Call next handler
 		next(w, r)
 	}
-}
-
-// authenticate validates an incoming http Request.
-// It will check over what protocol it came, what type of request it is and
-// will validate the TLS certificate.
-//
-// This does not perform authorization, only validates authentication.
-// Returns whether trusted or not, the username (or certificate fingerprint) of the trusted client, and the type of
-// client that has been authenticated (unix or tls).
-func (a Authenticator) authenticate(w http.ResponseWriter, r *http.Request) (bool, string, string, error) {
-	// Local unix socket queries.
-	if r.RemoteAddr == "@" && r.TLS == nil {
-		return true, "", "unix", nil
-	}
-
-	// Bad query, no TLS found.
-	if r.TLS == nil {
-		return false, "", "", fmt.Errorf("Bad/missing TLS on network query")
-	}
-
-	// Check for JWT token signed by an OpenID Connect provider.
-	if a.oidcVerifier != nil && a.oidcVerifier.IsRequest(r) {
-		userName, err := a.oidcVerifier.Auth(r.Context(), w, r)
-		if err != nil {
-			return false, "", "", err
-		}
-
-		return true, userName, api.AuthenticationMethodOIDC, nil
-	}
-
-	for _, cert := range r.TLS.PeerCertificates {
-		trusted, username := tls.CheckTrustState(r.Context(), *cert, a.trustedTLSClientCertFingerprints)
-		if trusted {
-			return true, username, api.AuthenticationMethodTLS, nil
-		}
-	}
-
-	// Reject unauthorized.
-	return false, "", "", nil
 }
