e20c4f3019d869becaaf44fb29bb9b99f2e5e0e2
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2025-05-14 10:50:27 +0200

    operations-centerd: Improve logging for background tasks
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- cmd/operations-centerd/internal/api/daemon.go
+++ cmd/operations-centerd/internal/api/daemon.go
@@ -350,9 +350,12 @@ func (d *Daemon) Start(ctx context.Context) error {
 	})
 
 	refreshTask := func(ctx context.Context) {
+		slog.InfoContext(ctx, "Refresh updates triggered")
 		err := updateSvc.Refresh(ctx)
 		if err != nil {
 			slog.ErrorContext(ctx, "Refresh updates failed: %w", logger.Err(err))
+		} else {
+			slog.InfoContext(ctx, "Refresh updates completed")
 		}
 	}
 
