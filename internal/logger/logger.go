6b09eef1b5bb3539afde374b50827fdbf215194c
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2025-05-15 12:09:07 +0000

    logger: Improve handling of truncated log values
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -78,11 +78,15 @@ func InitLogger(writer io.Writer, filepath string, verbose bool, debug bool) err
 // of log values to the given limit.
 func logValueMaxSize(limit int) func(groups []string, attr slog.Attr) slog.Attr {
 	return func(groups []string, attr slog.Attr) slog.Attr {
+		if attr.Key == slog.LevelKey || attr.Key == slog.SourceKey {
+			return attr
+		}
+
 		switch attr.Value.Kind() {
 		case slog.KindAny, slog.KindString:
 			val := attr.Value.String()
 			if len(val) > limit {
-				val = val[:limit]
+				val = val[:limit] + "... (truncated)"
 			}
 
 			attr.Value = slog.StringValue(val)
