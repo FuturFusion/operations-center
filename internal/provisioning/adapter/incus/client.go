5aa68cd031950467fdbaac56fd1bf58617d6714a
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2025-06-17 11:12:49 +0200

    provisioning/adapter/incus: Implement methods to form an Incus cluster
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- internal/provisioning/adapter/incus/client.go
+++ internal/provisioning/adapter/incus/client.go
@@ -3,11 +3,15 @@ package incus
 import (
 	"context"
 	"encoding/json"
+	"encoding/pem"
 	"fmt"
+	"log/slog"
 	"net/http"
+	"net/url"
 
 	incusosapi "github.com/lxc/incus-os/incus-osd/api"
 	incus "github.com/lxc/incus/v6/client"
+	incusapi "github.com/lxc/incus/v6/shared/api"
 
 	"github.com/FuturFusion/operations-center/internal/provisioning"
 	"github.com/FuturFusion/operations-center/shared/api"
@@ -27,13 +31,27 @@ func New(clientCert string, clientKey string) client {
 	}
 }
 
-func (c client) getClient(ctx context.Context, server provisioning.Server) (incus.InstanceServer, error) {
-	return incus.ConnectIncusWithContext(ctx, server.ConnectionURL, &incus.ConnectionArgs{
+type getClientOpt func(*incus.ConnectionArgs)
+
+func getClientWithInsecureSkipVerify() getClientOpt {
+	return func(args *incus.ConnectionArgs) {
+		args.InsecureSkipVerify = true
+	}
+}
+
+func (c client) getClient(ctx context.Context, server provisioning.Server, opts ...getClientOpt) (incus.InstanceServer, error) {
+	args := &incus.ConnectionArgs{
 		TLSClientCert: c.clientCert,
 		TLSClientKey:  c.clientKey,
 		TLSServerCert: server.Certificate,
 		SkipGetServer: true,
-	})
+	}
+
+	for _, opt := range opts {
+		opt(args)
+	}
+
+	return incus.ConnectIncusWithContext(ctx, server.ConnectionURL, args)
 }
 
 func (c client) Ping(ctx context.Context, server provisioning.Server) error {
@@ -43,8 +61,11 @@ func (c client) Ping(ctx context.Context, server provisioning.Server) error {
 	}
 
 	_, _, err = client.GetServer()
+	if err != nil {
+		return fmt.Errorf("Failed to ping %q: %w", server.ConnectionURL, err)
+	}
 
-	return err
+	return nil
 }
 
 func (c client) GetResources(ctx context.Context, server provisioning.Server) (api.HardwareData, error) {
@@ -106,6 +127,192 @@ func (c client) UpdateNetworkConfig(ctx context.Context, server provisioning.Ser
 	_, _, err = client.RawQuery(http.MethodPut, "/os/1.0/system/network", server.OSData.Network, "")
 	if err != nil {
 		return fmt.Errorf("Put OS network data to %q failed: %w", server.ConnectionURL, err)
+	}
+
+	return nil
+}
+
+func (c client) EnableOSServiceLVM(ctx context.Context, server provisioning.Server) error {
+	client, err := c.getClient(ctx, server)
+	if err != nil {
+		return err
+	}
+
+	serviceLVMConfig := map[string]any{
+		"enabled":   true,
+		"system_id": server.ID,
+	}
+
+	_, _, err = client.RawQuery(http.MethodGet, "/os/1.0/services/lvm", serviceLVMConfig, "")
+	if err != nil {
+		return fmt.Errorf("Enable OS service LVM on %q failed: %w", server.ConnectionURL, err)
+	}
+
+	return nil
+}
+
+func (c client) SetClusterAddress(ctx context.Context, server provisioning.Server) error {
+	client, err := c.getClient(ctx, server)
+	if err != nil {
+		return err
+	}
+
+	// Ignore error, connection URL has been parsed by incus client already.
+	serverAddressURL, _ := url.Parse(server.ConnectionURL)
+
+	svr, etag, err := client.GetServer()
+	if err != nil {
+		return fmt.Errorf("Failed to get current server config from %q: %w", server.ConnectionURL, err)
+	}
+
+	if svr.Config == nil {
+		svr.Config = map[string]string{}
+	}
+
+	svr.Config["core.https_address"] = serverAddressURL.Host
+	svr.Config["cluster.https_address"] = serverAddressURL.Host
+
+	err = client.UpdateServer(svr.Writable(), etag)
+	if err != nil {
+		return fmt.Errorf("Failed to set server config on %q: %w", server.ConnectionURL, err)
+	}
+
+	return nil
+}
+
+func (c client) EnableCluster(ctx context.Context, server provisioning.Server) (clusterCertificate string, _ error) {
+	client, err := c.getClient(ctx, server)
+	if err != nil {
+		return "", err
+	}
+
+	req := incusapi.ClusterPut{
+		Cluster: incusapi.Cluster{
+			ServerName: server.Name,
+			Enabled:    true,
+		},
+	}
+
+	op, err := client.UpdateCluster(req, "")
+	if err != nil {
+		return "", fmt.Errorf("Failed to update cluster on %q: %w", server.ConnectionURL, err)
+	}
+
+	err = op.WaitContext(ctx)
+	if err != nil {
+		return "", fmt.Errorf("Failed to update cluster on %q: %w", server.ConnectionURL, err)
+	}
+
+	anyClusterCertificate, ok := op.Get().Metadata["certificate"]
+	if !ok {
+		return "", nil
+	}
+
+	clusterCertificate, ok = anyClusterCertificate.(string)
+	if !ok {
+		return "", nil
+	}
+
+	return clusterCertificate, nil
+}
+
+func (c client) InsecureGetClusterCertificate(ctx context.Context, cluster provisioning.Server) (clusterCertificate string, _ error) {
+	slog.WarnContext(ctx, "Cluster certificate is fetched insecurely", slog.String("cluster", cluster.Name), slog.String("connection_url", cluster.ConnectionURL))
+
+	client, err := c.getClient(ctx, cluster, getClientWithInsecureSkipVerify())
+	if err != nil {
+		return "", err
+	}
+
+	// Ignore error, connection URL has been parsed by incus client already.
+	clusterURL, _ := url.Parse(cluster.ConnectionURL + "/1.0")
+
+	resp, err := client.DoHTTP(&http.Request{
+		Method: http.MethodGet,
+		URL:    clusterURL,
+		Body:   http.NoBody,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	certificate := pem.EncodeToMemory(&pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: resp.TLS.PeerCertificates[0].Raw,
+	})
+
+	return string(certificate), nil
+}
+
+func (c client) GetClusterNodeNames(ctx context.Context, server provisioning.Server) ([]string, error) {
+	client, err := c.getClient(ctx, server)
+	if err != nil {
+		return nil, err
+	}
+
+	nodeNames, err := client.GetClusterMemberNames()
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get cluster node names: %w", err)
+	}
+
+	return nodeNames, nil
+}
+
+func (c client) GetClusterJoinToken(ctx context.Context, server provisioning.Server, memberName string) (joinToken string, _ error) {
+	client, err := c.getClient(ctx, server)
+	if err != nil {
+		return "", err
+	}
+
+	op, err := client.CreateClusterMember(incusapi.ClusterMembersPost{
+		ServerName: memberName,
+	})
+	if err != nil {
+		return "", fmt.Errorf("Failed to get cluster join token on %q: %w", server.ConnectionURL, err)
+	}
+
+	opAPI := op.Get()
+	token, err := opAPI.ToClusterJoinToken()
+	if err != nil {
+		return "", fmt.Errorf("Failed converting token operation to join token: %w", err)
+	}
+
+	return token.String(), nil
+}
+
+func (c client) JoinCluster(ctx context.Context, server provisioning.Server, joinToken string, cluster provisioning.Server) error {
+	client, err := c.getClient(ctx, server)
+	if err != nil {
+		return err
+	}
+
+	// Ignore error, connection URL has been parsed by incus client already.
+	serverAddressURL, _ := url.Parse(server.ConnectionURL)
+	clusterAddressURL, _ := url.Parse(cluster.ConnectionURL)
+
+	op, err := client.UpdateCluster(incusapi.ClusterPut{
+		Cluster: incusapi.Cluster{
+			ServerName: server.Name,
+			Enabled:    true,
+			// TODO: Add storage pool config?
+			MemberConfig: []incusapi.ClusterMemberConfigKey{},
+		},
+		ClusterCertificate: cluster.Certificate,
+		ServerAddress:      serverAddressURL.Host,
+		ClusterToken:       joinToken,
+		ClusterAddress:     clusterAddressURL.Host,
+	}, "")
+	if err != nil {
+		return fmt.Errorf("Failed to update cluster during cluster join on %q: %w", server.ConnectionURL, err)
+	}
+
+	err = op.WaitContext(ctx)
+	if err != nil {
+		return fmt.Errorf("Failed to wait for update operation during cluster join on %q: %w", server.ConnectionURL, err)
 	}
 
 	return nil
