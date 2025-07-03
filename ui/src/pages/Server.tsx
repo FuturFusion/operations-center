9bc1dbb776de5e3b81207b88d1a7f54c22d3ec5e
Piotr Resztak <piotr.resztak@futurfusion.io>
2025-07-03 23:31:32 +0200

    ui: Add servers view
    Signed-off-by: Piotr Resztak <piotr.resztak@futurfusion.io>

---
+++ ui/src/pages/Server.tsx
@@ -0,0 +1,76 @@
+import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router";
+import { fetchServers } from "api/server";
+import DataTable from "components/DataTable";
+
+const Server = () => {
+  const {
+    data: servers = [],
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: ["servers"],
+    queryFn: fetchServers,
+  });
+
+  if (isLoading) {
+    return <div>Loading servers...</div>;
+  }
+
+  if (error) {
+    return <div>Error while loading servers: {error.message}</div>;
+  }
+
+  const headers = ["Name", "Cluster", "Connection URL", "Type", "Status"];
+  const rows = servers.map((item) => {
+    return [
+      {
+        content: (
+          <Link
+            to={`/ui/provisioning/servers/${item.name}`}
+            className="data-table-link"
+          >
+            {item.name}
+          </Link>
+        ),
+        sortKey: item.name,
+      },
+      {
+        content: item.cluster,
+        sortKey: item.cluster,
+      },
+      {
+        content: (
+          <Link
+            to={`${item.connection_url}`}
+            target="_blank"
+            className="data-table-link"
+          >
+            {item.connection_url}
+          </Link>
+        ),
+        sortKey: item.connection_url,
+      },
+      {
+        content: item.server_type,
+        sortKey: item.server_type,
+      },
+      {
+        content: item.server_status,
+        sortKey: item.server_status,
+      },
+    ];
+  });
+
+  return (
+    <>
+      <div className="d-flex flex-column">
+        <div className="scroll-container flex-grow-1">
+          <DataTable headers={headers} rows={rows} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Server;
