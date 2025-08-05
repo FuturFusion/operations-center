06fbc5524561acc472c0a65a2ddeb312f44709a7
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2025-08-05 11:20:50 +0200

    api: Add http api test for system/certificate
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

---
+++ internal/api/api_system_certificate_internal_test.go
@@ -0,0 +1,91 @@
+package api
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/FuturFusion/operations-center/internal/authn"
+	systemMock "github.com/FuturFusion/operations-center/internal/system/mock"
+	"github.com/FuturFusion/operations-center/internal/testing/boom"
+)
+
+func Test_systemHandler_certificatePut(t *testing.T) {
+	tests := []struct {
+		name                              string
+		requestBody                       string
+		systemServiceUpdateCertificateErr error
+
+		wantStatus              int
+		wantResponseBodyContain string
+	}{
+		{
+			name:        "success",
+			requestBody: `{"certificate": "-----BEGIN CERTIFICATE-----\n...\n-----END CERTIFICATE-----", "key": "-----BEGIN PRIVATE KEY-----\n...\n-----END PRIVATE KEY-----"}`,
+
+			wantStatus:              http.StatusOK,
+			wantResponseBodyContain: "Success",
+		},
+		{
+			name:        "error - invalid request body",
+			requestBody: `invalid`,
+
+			wantStatus:              http.StatusBadRequest,
+			wantResponseBodyContain: "invalid character 'i'",
+		},
+		{
+			name:                              "error - systemService.UpdateCertificate",
+			requestBody:                       `{"certificate": "-----BEGIN CERTIFICATE-----\n...\n-----END CERTIFICATE-----", "key": "-----BEGIN PRIVATE KEY-----\n...\n-----END PRIVATE KEY-----"}`,
+			systemServiceUpdateCertificateErr: boom.Error,
+
+			wantStatus:              http.StatusInternalServerError,
+			wantResponseBodyContain: "boom!",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			// Setup
+			authenticator := authn.New([]authn.Auther{dummyAuthenticator{}})
+
+			serveMux := http.NewServeMux()
+			router := newRouter(serveMux).AddMiddlewares(
+				authenticator.Middleware,
+			)
+
+			systemService := &systemMock.SystemServiceMock{
+				UpdateCertificateFunc: func(ctx context.Context, cert, key string) error {
+					return tc.systemServiceUpdateCertificateErr
+				},
+			}
+
+			registerSystemHandler(router, noopAuthorizer{}, systemService)
+
+			server := httptest.NewServer(serveMux)
+			t.Cleanup(func() {
+				server.Close()
+			})
+
+			// Execute http request
+			req, err := http.NewRequest(http.MethodPut, server.URL+"/certificate", bytes.NewBufferString(tc.requestBody))
+			require.NoError(t, err)
+
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			require.NoError(t, err)
+			defer resp.Body.Close()
+
+			body, err := io.ReadAll(resp.Body)
+			require.NoError(t, err)
+
+			// Assert results
+			require.Equal(t, tc.wantStatus, resp.StatusCode)
+			require.Contains(t, string(body), tc.wantResponseBodyContain)
+		})
+	}
+}
---
+++ internal/api/helpers_test.go
@@ -0,0 +1,20 @@
+package api
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/FuturFusion/operations-center/internal/authz"
+)
+
+type dummyAuthenticator struct{}
+
+func (d dummyAuthenticator) Auth(w http.ResponseWriter, r *http.Request) (bool, string, string, error) {
+	return true, "testuser", "testprotocol", nil
+}
+
+type noopAuthorizer struct{}
+
+func (n noopAuthorizer) CheckPermission(ctx context.Context, details *authz.RequestDetails, object authz.Object, entitlement authz.Entitlement) error {
+	return nil
+}
