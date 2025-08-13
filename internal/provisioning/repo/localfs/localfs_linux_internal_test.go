88b9d9ab8dd53f69d76b32fb6ece1f15f3f133cc
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2025-08-13 15:56:35 +0200

    provisioning/localfs: Add method UsageInformation
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

---
+++ internal/provisioning/repo/localfs/localfs_linux_internal_test.go
@@ -0,0 +1,41 @@
+//go:build linux
+
+package localfs
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestLocalfs_UsageInformation(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "success",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			// Setup
+			tmpDir := t.TempDir()
+			// tc.setupTmpDir(t, tmpDir)
+			lfs, err := New(tmpDir, nil)
+			require.NoError(t, err)
+
+			// Run test
+			gotUI, err := lfs.UsageInformation(context.Background())
+
+			// Assert
+			require.NoError(t, err)
+			require.Positive(t, gotUI.TotalSpaceBytes)
+			require.Positive(t, gotUI.AvailableSpaceBytes)
+			require.Positive(t, gotUI.UsedSpaceBytes)
+			require.Equal(t, gotUI.TotalSpaceBytes, gotUI.AvailableSpaceBytes+gotUI.UsedSpaceBytes)
+			t.Log(gotUI)
+		})
+	}
+}
