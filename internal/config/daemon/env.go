a374d5da421557e1558ef5c058101bf35f93080a
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2025-09-15 10:25:41 +0200

    config: Add env var to disable background tasks
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- internal/config/daemon/env.go
+++ internal/config/daemon/env.go
@@ -5,6 +5,19 @@ import (
 	"strconv"
 )
 
+// IsBackgroundTasksDisabled checks OPERATIONS_CENTER_DISABLE_BACKGROUND_TASKS
+// env var. If the env var has a value indicating true ("1", "t", "T", "true",
+// "TRUE", "True"), true is returned. False is returned otherwise.
+//
+// If true, all background tasks are disabled. This is mainly useful during
+// development or for integration tests.
+func IsBackgroundTasksDisabled() bool {
+	env := os.Getenv(ApplicationEnvPrefix + "_DISABLE_BACKGROUND_TASKS")
+
+	value, _ := strconv.ParseBool(env)
+	return value
+}
+
 // SourcePollSkipFirst checks OPERATIONS_CENTER_SOURCE_POLL_SKIP_FIRST env var.
 // If the env var has a value indicating true ("1", "t", "T", "true", "TRUE",
 // "True"), true is returned. False is returned otherwise.
