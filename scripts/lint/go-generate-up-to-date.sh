3f71830c05deeefa5b701bb6d4e6a68e29a53587
Piotr Resztak <piotr.resztak@gmail.com>
2025-10-15 10:40:54 +0200

    Update scripts/lint/go-generate-up-to-date.sh
    Co-authored-by: Lucas Bremgartner <breml@users.noreply.github.com>
    Signed-off-by: Piotr Resztak <piotr.resztak@gmail.com>

--- scripts/lint/go-generate-up-to-date.sh
+++ scripts/lint/go-generate-up-to-date.sh
@@ -3,7 +3,7 @@
 go generate ./...
 
 if [ -n "$(git status --porcelain -- '**/*_gen.go' '**/*_gen_test.go' '**/*_gen.tsx' '**/*_gen.d.ts')" ]; then
-  git status -- '**/*_gen.go' '**/*_gen.tsx' '**/*_gen.d.ts'
+  git status -- '**/*_gen.go' '**/*_gen_test.go' '**/*_gen.tsx' '**/*_gen.d.ts'
   exit 1
 fi
 
