b416ba3405240721504d1136d5bebf5de2157fa5
Piotr Resztak <piotr.resztak@futurfusion.io>
2025-10-16 15:16:23 +0200

    ui: Add support for cluster creation from template
    Signed-off-by: Piotr Resztak <piotr.resztak@futurfusion.io>

--- ui/src/pages/ClusterCreate.tsx
+++ ui/src/pages/ClusterCreate.tsx
@@ -1,16 +1,40 @@
 import { useNavigate, useParams } from "react-router";
+import { createCluster } from "api/cluster";
 import TabView from "components/TabView";
-import ClusterCreateManual from "pages/ClusterCreateManual";
+import ClusterCreateForm from "components/ClusterCreateForm";
+import { useNotification } from "context/notificationContext";
+import { ClusterPost } from "types/cluster";
 
 const ClusterCreate = () => {
+  const { notify } = useNotification();
   const navigate = useNavigate();
   const { activeTab } = useParams<{ activeTab: string }>();
+
+  const onSubmit = (values: ClusterPost) => {
+    return createCluster(JSON.stringify(values, null, 2))
+      .then((response) => {
+        if (response.error_code == 0) {
+          notify.success(`Cluster ${values.name} created`);
+          navigate(`/ui/provisioning/clusters`);
+          return;
+        }
+        notify.error(response.error);
+      })
+      .catch((e) => {
+        notify.error(`Error during cluster creation: ${e}`);
+      });
+  };
 
   const tabs = [
     {
       key: "manual",
       title: "Manually",
-      content: <ClusterCreateManual />,
+      content: <ClusterCreateForm mode="manual" onSubmit={onSubmit} />,
+    },
+    {
+      key: "template",
+      title: "From template",
+      content: <ClusterCreateForm mode="template" onSubmit={onSubmit} />,
     },
   ];
 
--- ui/src/types/cluster.d.ts
+++ ui/src/types/cluster.d.ts
@@ -5,6 +5,15 @@ export interface Cluster {
   last_updated: string;
 }
 
+export interface ClusterPost {
+  name: string;
+  connection_url: string;
+  server_names: string[];
+  server_type: string;
+  services_config: YamlValue;
+  application_seed_config: YamlValue;
+}
+
 export interface ClusterFormValues {
   name: string;
   connection_url: string;
@@ -12,6 +21,8 @@ export interface ClusterFormValues {
   server_type: string;
   services_config: YamlValue;
   application_seed_config: YamlValue;
+  variables: string;
+  template: string;
 }
 
 export interface ClusterCertFormValues {
