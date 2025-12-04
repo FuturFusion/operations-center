9e89f81d4a10140de4f951cb8ea7c7c1e03f8063
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2025-12-04 14:30:43 +0100

    inventory/repo/sqlite/entities: Add boilerplate
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

---
+++ internal/inventory/repo/sqlite/entities/cluster.go
@@ -0,0 +1,11 @@
+package entities
+
+// Code generation directives.
+//
+//generate-database:mapper target cluster.mapper.go
+//generate-database:mapper reset
+//
+//generate-database:mapper stmt -e cluster objects
+//generate-database:mapper stmt -e cluster id
+//
+//generate-database:mapper method -e cluster ID
