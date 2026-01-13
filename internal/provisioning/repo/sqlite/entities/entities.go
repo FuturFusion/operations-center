97aec0ceb0329c45b221c3c6ca5b7e615477c38b
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2026-01-13 13:05:17 +0100

    entities: Fix import path for local packages
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- internal/provisioning/repo/sqlite/entities/entities.go
+++ internal/provisioning/repo/sqlite/entities/entities.go
@@ -2,4 +2,4 @@ package entities
 
 //go:generate go run github.com/lxc/incus/v6/cmd/generate-database db mapper generate -b mapper_boilerplate.go -p "github.com/FuturFusion/operations-center/internal/provisioning/repo/sqlite/entities" -p "github.com/FuturFusion/operations-center/internal/provisioning"
 //go:generate gofmt -s -w .
-//go:generate go run golang.org/x/tools/cmd/goimports -w .
+//go:generate go run golang.org/x/tools/cmd/goimports -w -local github.com/FuturFusion/operations-center .
