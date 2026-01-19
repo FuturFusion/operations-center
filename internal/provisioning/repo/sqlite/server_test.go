1265745c4113b6386c987fd30784432f7dba3531
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2026-01-19 13:37:34 +0100

    provisioning/repo/sqlite: Update tests
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- internal/provisioning/repo/sqlite/server_test.go
+++ internal/provisioning/repo/sqlite/server_test.go
@@ -5,6 +5,7 @@ import (
 	"crypto/tls"
 	"testing"
 
+	incusosapi "github.com/lxc/incus-os/incus-osd/api"
 	incustls "github.com/lxc/incus/v6/shared/tls"
 	"github.com/stretchr/testify/require"
 
@@ -64,7 +65,37 @@ func TestServerDatabaseActions(t *testing.T) {
 		},
 	}
 
-	client := &adapterMock.ClusterClientPortMock{
+	serverClient := &adapterMock.ServerClientPortMock{
+		GetUpdateConfigFunc: func(ctx context.Context, server provisioning.Server) (provisioning.ServerSystemUpdate, error) {
+			return provisioning.ServerSystemUpdate{
+				Config: incusosapi.SystemUpdateConfig{
+					AutoReboot:     false,
+					Channel:        "stable",
+					CheckFrequency: "never",
+				},
+			}, nil
+		},
+		UpdateUpdateConfigFunc: func(ctx context.Context, server provisioning.Server, providerConfig provisioning.ServerSystemUpdate) error {
+			return nil
+		},
+		PingFunc: func(ctx context.Context, endpoint provisioning.Endpoint) error {
+			return nil
+		},
+		GetResourcesFunc: func(ctx context.Context, endpoint provisioning.Endpoint) (api.HardwareData, error) {
+			return api.HardwareData{}, nil
+		},
+		GetOSDataFunc: func(ctx context.Context, endpoint provisioning.Endpoint) (api.OSData, error) {
+			return api.OSData{}, nil
+		},
+		GetVersionDataFunc: func(ctx context.Context, endpoint provisioning.Endpoint) (api.ServerVersionData, error) {
+			return api.ServerVersionData{}, nil
+		},
+		GetServerTypeFunc: func(ctx context.Context, endpoint provisioning.Endpoint) (api.ServerType, error) {
+			return api.ServerTypeIncus, nil
+		},
+	}
+
+	clusterClient := &adapterMock.ClusterClientPortMock{
 		PingFunc: func(ctx context.Context, endpoint provisioning.Endpoint) error {
 			return nil
 		},
@@ -119,10 +150,12 @@ func TestServerDatabaseActions(t *testing.T) {
 	entities.PreparedStmts, err = entities.PrepareStmts(tx, false)
 	require.NoError(t, err)
 
-	server := sqlite.NewServer(tx)
-	serverSvc := provisioning.NewServerService(server, nil, nil, nil, nil, "", tls.Certificate{})
+	cannelSvc := provisioning.NewChannelService(sqlite.NewChannel(db), nil)
 
-	clusterSvc := provisioning.NewClusterService(sqlite.NewCluster(db), localArtifactRepo, client, serverSvc, nil, nil, terraformProvisioner)
+	server := sqlite.NewServer(tx)
+	serverSvc := provisioning.NewServerService(server, serverClient, nil, nil, cannelSvc, "", tls.Certificate{})
+
+	clusterSvc := provisioning.NewClusterService(sqlite.NewCluster(db), localArtifactRepo, clusterClient, serverSvc, nil, nil, terraformProvisioner)
 
 	// Add server
 	_, err = server.Create(ctx, serverA)
