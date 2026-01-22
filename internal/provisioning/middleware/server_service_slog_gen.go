f88fd2156b5679263bab151c4f430474f45bfae7
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2026-01-22 08:14:42 +0100

    provisioning: Update generated code
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- internal/provisioning/middleware/server_service_slog_gen.go
+++ internal/provisioning/middleware/server_service_slog_gen.go
@@ -613,12 +613,13 @@ func (_d ServerServiceWithSlog) SetClusterService(clusterSvc provisioning.Cluste
 }
 
 // Update implements provisioning.ServerService.
-func (_d ServerServiceWithSlog) Update(ctx context.Context, server provisioning.Server) (err error) {
+func (_d ServerServiceWithSlog) Update(ctx context.Context, server provisioning.Server, updateSystem bool) (err error) {
 	log := _d._log.With()
 	if _d._log.Enabled(ctx, logger.LevelTrace) {
 		log = log.With(
 			slog.Any("ctx", ctx),
 			slog.Any("server", server),
+			slog.Bool("updateSystem", updateSystem),
 		)
 	}
 	log.DebugContext(ctx, "=> calling Update")
@@ -643,7 +644,7 @@ func (_d ServerServiceWithSlog) Update(ctx context.Context, server provisioning.
 			log.DebugContext(ctx, "<= method Update finished")
 		}
 	}()
-	return _d._base.Update(ctx, server)
+	return _d._base.Update(ctx, server, updateSystem)
 }
 
 // UpdateSystemNetwork implements provisioning.ServerService.
