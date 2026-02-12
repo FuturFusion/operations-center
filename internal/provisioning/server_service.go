405590fe504ead6e1eeddbf1ce9c79cbe65f4d36
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2026-02-12 17:54:58 +0100

    provisioning: Fix some linting issues
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- internal/provisioning/server_service.go
+++ internal/provisioning/server_service.go
@@ -734,7 +734,6 @@ func (s *serverService) SelfRegisterOperationsCenter(ctx context.Context) error 
 
 		if len(servers) == 0 {
 			// Create server entry
-
 			server = Server{
 				Name:                api.ServerNameOperationsCenter,
 				Type:                api.ServerTypeOperationsCenter,
@@ -754,7 +753,6 @@ func (s *serverService) SelfRegisterOperationsCenter(ctx context.Context) error 
 			pollAfterCreate = true
 		} else {
 			// Update existing server entry
-
 			server = servers[0]
 			server.ConnectionURL = connectionURL
 			server.PublicConnectionURL = config.GetNetwork().OperationsCenterAddress
