9a25bf827be0789c60f9d9f85d5d81ef2a704608
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2026-02-12 16:38:40 +0100

    provisioning: Update GetVersionData signature
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- internal/provisioning/server_ports.go
+++ internal/provisioning/server_ports.go
@@ -58,7 +58,7 @@ type ServerClientPort interface {
 	Ping(ctx context.Context, endpoint Endpoint) error
 	GetResources(ctx context.Context, endpoint Endpoint) (api.HardwareData, error)
 	GetOSData(ctx context.Context, endpoint Endpoint) (api.OSData, error)
-	GetVersionData(ctx context.Context, endpoint Endpoint) (api.ServerVersionData, error)
+	GetVersionData(ctx context.Context, server Server) (api.ServerVersionData, error)
 	GetServerType(ctx context.Context, endpoint Endpoint) (api.ServerType, error)
 	UpdateNetworkConfig(ctx context.Context, server Server) error
 	UpdateStorageConfig(ctx context.Context, server Server) error
