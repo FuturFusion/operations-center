c24396a86c4c2528e1fd481cd2eee36d9b49ed8b
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2026-03-11 15:37:33 +0100

    util/testing/queue: add helpers for err only
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- internal/util/testing/queue/queue.go
+++ internal/util/testing/queue/queue.go
@@ -29,6 +29,22 @@ func Pop[T any](t testingT, queue *[]Item[T]) (T, error) {
 	return ret, err
 }
 
+// PopErr removes the next item from the queue and returns only the error
+// from it.
+func PopErr[T any](t testingT, queue *[]Item[T]) error {
+	t.Helper()
+
+	if len(*queue) == 0 {
+		t.Fatal("queue already drained")
+	}
+
+	_, err := (*queue)[0].Value, (*queue)[0].Err
+	updatedQueue := (*queue)[1:]
+	*queue = updatedQueue
+
+	return err
+}
+
 // PopRetainLast removes the next item from the queue and returns it.
 // The last item is not removed and therefore returned on every subsequent call.
 func PopRetainLast[T any](t testingT, queue *[]Item[T]) (T, error) {
@@ -45,4 +61,21 @@ func PopRetainLast[T any](t testingT, queue *[]Item[T]) (T, error) {
 	}
 
 	return ret, err
+}
+
+type Errs []error
+
+// Pop removes the first item from the error queue and returns it.
+func (e *Errs) Pop(t *testing.T) error {
+	t.Helper()
+
+	if len(*e) == 0 {
+		t.Fatal("queue already drained")
+	}
+
+	err := (*e)[0]
+	updatedErrs := (*e)[1:]
+	*e = updatedErrs
+
+	return err
 }
