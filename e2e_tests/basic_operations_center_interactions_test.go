83e721ea62f6fac6507cc9ae3b35fd65b5612f93
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2026-04-08 10:52:28 +0200

    e2e-tests: test server registration scriptlet
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- e2e_tests/basic_operations_center_interactions_test.go
+++ e2e_tests/basic_operations_center_interactions_test.go
@@ -5,6 +5,8 @@ import "testing"
 func basicOperationsCenterInteractions(t *testing.T, tmpDir string) {
 	t.Helper()
 
+	assertServerRegistrationScriptletEffects(t)
+
 	assertOperationsCenterCliAdmin(t)
 	assertOperationsCenterCliQuery(t)
 	assertOperationsCenterCliSystem(t)
