542b59a3579f055708b850a737fe82d121fdc784
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2026-05-05 06:15:41 +0200

    api: change server removal api to support multiple servers
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- shared/api/provisioning_cluster.go
+++ shared/api/provisioning_cluster.go
@@ -392,14 +392,14 @@ type ClusterAddServersPost struct {
 	SkipPostJoinOperations bool `json:"skip_post_join_operations" yaml:"skip_post_join_operations"`
 }
 
-// ClusterRemoveServerPost represents a remove server from a cluster request
+// ClusterRemoveServersPost represents a remove server from a cluster request
 // containing the name of the server to be removed from the cluster.
 //
 // swagger:model
-type ClusterRemoveServerPost struct {
+type ClusterRemoveServersPost struct {
 	// Name of the server to be removed from the cluster.
 	// Example: "server1"
-	ServerName string `json:"server_name" yaml:"server_name"`
+	ServerNames []string `json:"server_names" yaml:"server_names"`
 }
 
 type ClusterMemberConfigKey = incusapi.ClusterMemberConfigKey
