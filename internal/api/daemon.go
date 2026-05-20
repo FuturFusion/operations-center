2e16310f8d9019101d17ec6cf19f9003737098f7
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2026-05-20 15:42:01 +0200

    api: add simplestream public api endpoints
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- internal/api/daemon.go
+++ internal/api/daemon.go
@@ -945,6 +945,8 @@ func (d *Daemon) setupAPIRoutes(
 		return true
 	}
 
+	simplestreamsRouter := router.SubGroup("/incus-images")
+
 	api10router := router.SubGroup("/1.0").AddMiddlewares(
 		d.authenticator.Middleware(authn.WithIsAuthenticationRequired(isAuthenticationRequired)),
 	)
@@ -952,7 +954,7 @@ func (d *Daemon) setupAPIRoutes(
 
 	imageRouter := api10router.SubGroup("/image")
 	imageIncusRouter := imageRouter.SubGroup("/incus")
-	registerImageIncusHandler(imageIncusRouter, d.authorizer, incusImageSvc)
+	registerImageIncusHandler(imageIncusRouter, simplestreamsRouter, d.authorizer, incusImageSvc)
 
 	internalRouter := api10router.SubGroup("/internal")
 	registerInternalHandler(internalRouter, d.authorizer, db)
