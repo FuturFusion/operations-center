61534173b69531af5ae18224b63b998a41807b80
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2026-06-02 10:35:06 +0200

    provisioning/adapter/terraform: use provided node specific config keys
    replace the manually added constants
    
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- internal/provisioning/adapter/terraform/funcs_node_specific_config.go
+++ internal/provisioning/adapter/terraform/funcs_node_specific_config.go
@@ -2,94 +2,45 @@ package terraform
 
 import (
 	"reflect"
-	"slices"
 )
-
-// nodeSpecificConfig lists all config keys, which are node-specific.
-// Extracted from https://github.com/lxc/incus/blob/5c9f4d6823b79cc0a8825c8c52aa3fe186f4e7bb/internal/server/node/config.go#L168
-var nodeSpecificConfig = []string{
-	"cluster.https_address",
-	"core.bgp_address",
-	"core.bgp_routerid",
-	"core.debug_address",
-	"core.dns_address",
-	"core.https_address",
-	"core.metrics_address",
-	"core.storage_buckets_address",
-	"core.syslog_socket",
-	"storage.backups_volume",
-	"storage.images_volume",
-	"storage.logs_volume",
-}
-
-// nodeSpecificStorageConfig lists all storage pool config keys which are node-specific.
-// This is copied from https://github.com/lxc/incus/blob/5c9f4d6823b79cc0a8825c8c52aa3fe186f4e7bb/internal/server/db/storage_pools.go#L885-L894
-var nodeSpecificStorageConfig = []string{
-	"source",
-	"source.wipe",
-	"zfs.pool_name",
-	"lvm.thinpool_name",
-	"lvm.vg_name",
-	"lvm.vg.force_reuse",
-}
-
-// nodeSpecificNetworkConfig lists all static network config keys which are node-specific.
-// This is copied from https://github.com/lxc/incus/blob/5c9f4d6823b79cc0a8825c8c52aa3fe186f4e7bb/internal/server/db/networks.go#L889-L895
-var nodeSpecificNetworkConfig = []string{
-	"bgp.ipv4.nexthop",
-	"bgp.ipv6.nexthop",
-	"bridge.external_interfaces",
-	"parent",
-}
 
 type splitConfigs struct {
 	Specific map[string]string
 	Global   map[string]string
 }
 
-func splitConfig(m any, kind string) splitConfigs {
-	v := reflect.ValueOf(m)
+func splitConfig(nodeSpecificConfigKeys map[string]map[string]bool) func(m any, entity string) splitConfigs {
+	return func(m any, entity string) splitConfigs {
+		v := reflect.ValueOf(m)
 
-	if v.Kind() != reflect.Map {
-		panic("config is not a map")
-	}
-
-	if v.Type().Key().Kind() != reflect.String {
-		panic("config key is not string")
-	}
-
-	var lookup []string
-	switch kind {
-	case "node":
-		lookup = nodeSpecificConfig
-
-	case "storage":
-		lookup = nodeSpecificStorageConfig
-
-	case "network":
-		lookup = nodeSpecificNetworkConfig
-
-	default:
-		panic("kind not supported, allowed values: node, storage, network")
-	}
-
-	specific := map[string]string{}
-	global := map[string]string{}
-
-	iter := reflect.ValueOf(m).MapRange()
-	for iter.Next() {
-		k := iter.Key()
-		v := iter.Value()
-
-		if slices.Contains(lookup, k.String()) {
-			specific[k.String()] = v.String()
-		} else {
-			global[k.String()] = v.String()
+		if v.Kind() != reflect.Map {
+			panic("config is not a map")
 		}
-	}
 
-	return splitConfigs{
-		Specific: specific,
-		Global:   global,
+		if v.Type().Key().Kind() != reflect.String {
+			panic("config key is not string")
+		}
+
+		lookup := nodeSpecificConfigKeys[entity]
+
+		specific := map[string]string{}
+		global := map[string]string{}
+
+		iter := reflect.ValueOf(m).MapRange()
+		for iter.Next() {
+			k := iter.Key()
+			v := iter.Value()
+
+			if lookup[k.String()] {
+				specific[k.String()] = v.String()
+			} else {
+				global[k.String()] = v.String()
+			}
+		}
+
+		return splitConfigs{
+			Specific: specific,
+			Global:   global,
+		}
 	}
 }
