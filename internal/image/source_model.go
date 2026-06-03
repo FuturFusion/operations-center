10e25593216c8099687af72aaa3e30b2898b9740
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2026-06-03 14:59:40 +0200

    image: add image source service
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

---
+++ internal/image/source_model.go
@@ -0,0 +1,55 @@
+package image
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"time"
+
+	"github.com/FuturFusion/operations-center/internal/domain"
+	"github.com/FuturFusion/operations-center/shared/api"
+)
+
+type ImageSource struct {
+	ID               int64               `json:"-"`
+	Name             string              `json:"name" db:"primary=yes"`
+	URL              string              `json:"url"`
+	Type             api.ImageSourceType `json:"type" db:"marshal=json"`
+	FilterExpression string              `json:"filter_expression"`
+
+	LastUpdated time.Time `json:"-" expr:"last_updated" db:"update_timestamp"`
+}
+
+func (s ImageSource) Validate(ctx context.Context, imageSourcers map[api.ImageSourceType]ImageSourcerPort) error {
+	if s.Name == "" {
+		return domain.NewValidationErrf("Invalid source, name can not be empty")
+	}
+
+	_, err := url.Parse(s.URL)
+	if err != nil {
+		return domain.NewValidationErrf("Invalid URL: %v", err)
+	}
+
+	if s.Type != api.ImageSourceTypeIncus {
+		return domain.NewValidationErrf(`Invalid source, only type "incus" is supported`)
+	}
+
+	imageSourcer, ok := imageSourcers[s.Type]
+	if !ok {
+		return fmt.Errorf("No source implementation available for source type %q", s.Type)
+	}
+
+	err = imageSourcer.ValidateFilterExpression(ctx, s.FilterExpression)
+	if err != nil {
+		return domain.NewValidationErrf("Invalid source, failed to validate filter expression: %v", err)
+	}
+
+	return nil
+}
+
+type ImageSourceFilter struct {
+	ID   *int
+	Name *string
+}
+
+type Sources []ImageSource
