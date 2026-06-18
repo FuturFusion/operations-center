8967c4af01dc2b5119e0299f4c998e1569b92f80
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2026-06-18 08:49:49 +0200

    provisioning/cluster: keep the primary application on delete with factory reset
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- internal/provisioning/cluster/cluster_service.go
+++ internal/provisioning/cluster/cluster_service.go
@@ -1573,14 +1573,23 @@ func (s *clusterService) DeleteAndFactoryResetByName(ctx context.Context, name s
 	}
 
 	if tokenSeedName == nil {
+		applications := make([]api.SeedApplication, 0, 1)
+		for _, application := range servers[0].VersionData.Applications {
+			if !domain.IsPrimaryApplication(application.Name) {
+				continue
+			}
+
+			applications = append(applications, api.SeedApplication{
+				Name: application.Name,
+			})
+
+			break
+		}
+
 		seed = provisioning.TokenImageSeedConfigs{
 			Applications: api.SeedApplications{
-				Version: "1",
-				Applications: []api.SeedApplication{
-					{
-						Name: "incus",
-					},
-				},
+				Version:      "1",
+				Applications: applications,
 			},
 			Incus: api.SeedIncus{
 				Version:       "1",
