35771580035811e3ca54921c091e62036a637c7e
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2026-07-02 10:20:49 +0200

    provisioning: update generated code (PreRegister)
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- internal/provisioning/server_expr_gen.go
+++ internal/provisioning/server_expr_gen.go
@@ -5,6 +5,7 @@ package provisioning
 import (
 	"time"
 
+	"github.com/google/uuid"
 	osapi "github.com/lxc/incus-os/incus-osd/api"
 
 	"github.com/FuturFusion/operations-center/shared/api"
@@ -377,6 +378,11 @@ type ExprServer struct {
 	StatusDetail         api.ServerStatusDetail   `json:"status_detail" expr:"status_detail"`
 	Description          string                   `json:"description" expr:"description"`
 	Properties           api.ConfigMap            `json:"properties" expr:"properties"`
+	BMCAPIType           api.BMCAPIType           `json:"bmc_api_type"           db:"sql=servers.bmc_api_type&marshal=json" expr:"bmc_api_type"`
+	BMCEndpoint          string                   `json:"bmc_endpoint" expr:"bmc_endpoint"`
+	BMCUsername          string                   `json:"bmc_username" expr:"bmc_username"`
+	BMCPassword          string                   `json:"bmc_password" expr:"bmc_password"`
+	RegistrationToken    uuid.UUID                `json:"registration_token" expr:"registration_token"`
 	LastUpdated          time.Time                `json:"last_updated"           db:"update_timestamp" expr:"last_updated"`
 	LastSeen             time.Time                `json:"last_seen" expr:"last_seen"`
 	LastStatusUpdated    time.Time                `json:"last_status_updated" expr:"last_status_updated"`
@@ -830,6 +836,11 @@ func ToExprServer(s Server) ExprServer {
 		StatusDetail:         s.StatusDetail,
 		Description:          s.Description,
 		Properties:           s.Properties,
+		BMCAPIType:           s.BMCAPIType,
+		BMCEndpoint:          s.BMCEndpoint,
+		BMCUsername:          s.BMCUsername,
+		BMCPassword:          s.BMCPassword,
+		RegistrationToken:    s.RegistrationToken,
 		LastUpdated:          s.LastUpdated,
 		LastSeen:             s.LastSeen,
 		LastStatusUpdated:    s.LastStatusUpdated,
