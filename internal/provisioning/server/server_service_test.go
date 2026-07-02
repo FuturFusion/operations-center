47a9da7e53b32a135f80040d00575eb2e7b17868
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2026-07-02 10:20:15 +0200

    provisioning/server: Add PreRegister
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- internal/provisioning/server/server_service_test.go
+++ internal/provisioning/server/server_service_test.go
@@ -206,6 +206,70 @@ func TestServerService_UpdateCertificate(t *testing.T) {
 	}
 }
 
+func TestServerService_PreRegister(t *testing.T) {
+	tests := []struct {
+		name          string
+		server        provisioning.Server
+		repoCreateErr error
+
+		assertErr require.ErrorAssertionFunc
+	}{
+		{
+			name: "success",
+			server: provisioning.Server{
+				Name:    "A",
+				Status:  api.ServerStatusUnregistered,
+				Channel: "stable",
+			},
+
+			assertErr: require.NoError,
+		},
+		{
+			name: "error - validation",
+			server: provisioning.Server{
+				Name:    "", // invalid
+				Status:  api.ServerStatusUnregistered,
+				Channel: "stable",
+			},
+
+			assertErr: func(tt require.TestingT, err error, a ...any) {
+				var verr domain.ErrValidation
+				require.ErrorAs(tt, err, &verr, a...)
+			},
+		},
+		{
+			name: "error - repo.Create",
+			server: provisioning.Server{
+				Name:    "A",
+				Status:  api.ServerStatusUnregistered,
+				Channel: "stable",
+			},
+			repoCreateErr: boom.Error,
+
+			assertErr: boom.ErrorIs,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			// Setup
+			repo := &repoMock.ServerRepoMock{
+				CreateFunc: func(ctx context.Context, newServer provisioning.Server) (int64, error) {
+					return -1, tc.repoCreateErr
+				},
+			}
+
+			serverSvc := provisioningServer.New(repo, nil, nil, nil, nil, nil, nil, tls.Certificate{})
+
+			// Run test
+			_, err := serverSvc.PreRegister(t.Context(), tc.server)
+
+			// Assert
+			tc.assertErr(t, err)
+		})
+	}
+}
+
 func TestServerService_Register(t *testing.T) {
 	config.InitTest(t, &envMock.EnvironmentMock{}, nil)
 
--- internal/provisioning/server_ports.go
+++ internal/provisioning/server_ports.go
@@ -11,6 +11,7 @@ import (
 
 type ServerService interface {
 	SetClusterService(clusterSvc ClusterService)
+	PreRegister(ctx context.Context, server Server) (Server, error)
 	Register(ctx context.Context, token uuid.UUID, server Server) (Server, error)
 	GetAll(ctx context.Context) (Servers, error)
 	GetAllWithFilter(ctx context.Context, filter ServerFilter) (Servers, error)
