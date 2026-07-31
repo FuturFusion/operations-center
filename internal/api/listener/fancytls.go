ba66eb5a50822c75f785677743a17061ee3fb81c
Stéphane Graber <stephane.graber@futurfusion.io>
2026-07-31 00:00:45 -0400

    api/listener: Update for new proxyproto defaults
    Signed-off-by: Stéphane Graber <stephane.graber@futurfusion.io>

--- internal/api/listener/fancytls.go
+++ internal/api/listener/fancytls.go
@@ -45,7 +45,7 @@ func (l *FancyTLSListener) Accept() (net.Conn, error) {
 	config := l.config
 
 	if isProxy(c.RemoteAddr().String(), l.trustedProxy) {
-		c = proxyproto.NewConn(c)
+		c = proxyproto.NewConn(c, proxyproto.WithPolicy(proxyproto.USE))
 	}
 
 	return tls.Server(c, config), nil
