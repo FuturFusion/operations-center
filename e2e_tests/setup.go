b063e258d4462625b9e724c79fc0e5af07040f2f
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2026-08-19 09:59:54 +0200

    e2e-tests: extend create incus instance timeout
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- e2e_tests/setup.go
+++ e2e_tests/setup.go
@@ -489,7 +489,7 @@ func createIncusOSInstances(t *testing.T, incusOSPreseededISOFilename string, na
 	existingServers, err := strconv.ParseInt(existingServersResp.OutputTrimmed(), 10, 64)
 	require.NoError(t, err)
 
-	timeout := 10 * time.Minute
+	timeout := 15*time.Minute + time.Duration(len(names))*3*time.Minute
 	if !concurrentSetup {
 		timeout = time.Duration(int(timeout) * len(names))
 	}
