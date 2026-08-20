ab10618e9bb56e0d54943a97f6b2fa5cba0615bd
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2026-08-20 10:47:41 +0200

    api: add GetSecureBootCertificates to env
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- internal/api/daemon.go
+++ internal/api/daemon.go
@@ -104,6 +104,7 @@ type environment interface {
 	UsrShareDir() string
 	IsIncusOS() bool
 	GetToken(ctx context.Context) (string, error)
+	GetSecureBootCertificates(ctx context.Context) (incusosapi.InternalSecureBootCertificates, error)
 }
 
 type Daemon struct {
@@ -775,7 +776,9 @@ func (d *Daemon) setupServerService(
 		provisioningServer.AddBMCServerClient(
 			api.BMCAPITypeRedfishV1Generic,
 			provisioningAdapterMiddleware.NewBMCServerClientPortWithSlog(
-				redfish.New(),
+				redfish.New(
+					redfish.WithSecureBootCertificates(d.env),
+				),
 			),
 		),
 	)
