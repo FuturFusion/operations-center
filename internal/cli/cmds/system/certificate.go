1787c7877697217fe582a406032355c512b9ceec
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2026-08-27 09:25:58 +0200

    cli: add system certificate show command
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- internal/cli/cmds/system/certificate.go
+++ internal/cli/cmds/system/certificate.go
@@ -1,9 +1,13 @@
 package system
 
 import (
+	"encoding/json"
+	"fmt"
 	"os"
+	"slices"
 
 	"github.com/spf13/cobra"
+	"go.yaml.in/yaml/v4"
 
 	"github.com/FuturFusion/operations-center/internal/cli/validate"
 	"github.com/FuturFusion/operations-center/internal/client"
@@ -21,12 +25,19 @@ func (c *CmdCertificate) Command() *cobra.Command {
 	cmd.Long = `Description:
   Interact with certificate config
 
-  Set server certificate for operations center.
+  Show and set server certificate for operations center.
 `
 
 	// Workaround for subcommand usage errors. See: https://github.com/spf13/cobra/issues/706
 	cmd.Args = cobra.NoArgs
 	cmd.Run = func(cmd *cobra.Command, args []string) { _ = cmd.Usage() }
+
+	// Show
+	certificateShowCmd := cmdCertificateShow{
+		ocClient: c.OCClient,
+	}
+
+	cmd.AddCommand(certificateShowCmd.Command())
 
 	// Set
 	certificateSetCmd := cmdCertificateSet{
@@ -36,6 +47,67 @@ func (c *CmdCertificate) Command() *cobra.Command {
 	cmd.AddCommand(certificateSetCmd.Command())
 
 	return cmd
+}
+
+// Show system server certificate.
+type cmdCertificateShow struct {
+	ocClient *client.OperationsCenterClient
+
+	flagFormat string
+}
+
+func (c *cmdCertificateShow) Command() *cobra.Command {
+	cmd := &cobra.Command{}
+	cmd.Use = "show"
+	cmd.Short = "Show server certificate"
+	cmd.Long = `Description:
+  Show server certificate
+
+  The corresponding private key is never shown.
+`
+
+	cmd.Flags().StringVarP(&c.flagFormat, "format", "f", "", `Format (json|yaml)`)
+
+	cmd.PreRunE = c.validateArgsAndFlags
+	cmd.RunE = c.run
+
+	return cmd
+}
+
+func (c *cmdCertificateShow) validateArgsAndFlags(cmd *cobra.Command, args []string) error {
+	// Quick checks.
+	exit, err := validate.Args(cmd, args, 0, 0)
+	if exit {
+		return err
+	}
+
+	validFormats := []string{"", "json", "yaml"}
+	if !slices.Contains(validFormats, c.flagFormat) {
+		return fmt.Errorf(`Invalid value for flag "--format": %q`, c.flagFormat)
+	}
+
+	return nil
+}
+
+func (c *cmdCertificateShow) run(cmd *cobra.Command, args []string) error {
+	certificate, err := c.ocClient.GetSystemCertificate(cmd.Context())
+	if err != nil {
+		return err
+	}
+
+	switch c.flagFormat {
+	case "json":
+		enc := json.NewEncoder(c.Command().OutOrStdout())
+		enc.SetIndent("", "  ")
+		err = enc.Encode(certificate)
+
+	default:
+		enc := yaml.NewEncoder(c.Command().OutOrStdout())
+		enc.SetIndent(2)
+		err = enc.Encode(certificate)
+	}
+
+	return err
 }
 
 // Set system server certificate.
