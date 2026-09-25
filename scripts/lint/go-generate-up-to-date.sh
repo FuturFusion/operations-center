2c2846005bd530bbf0b7cbaccaf6df8be9cce563
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2026-09-25 17:48:32 +0200

    linting: include server-deployment-states.md
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- scripts/lint/go-generate-up-to-date.sh
+++ scripts/lint/go-generate-up-to-date.sh
@@ -2,8 +2,8 @@
 
 go generate ./...
 
-if [ -n "$(git status --porcelain -- '**/*_gen.go' '**/*_gen_test.go' '**/*_gen.tsx' '**/*_gen.d.ts' '**/*.mapper.go' 'doc/reference/settings_log_components.md')" ]; then
-  git status -- '**/*_gen.go' '**/*_gen_test.go' '**/*_gen.tsx' '**/*_gen.d.ts' '**/*.mapper.go' 'doc/reference/settings_log_components.md'
+if [ -n "$(git status --porcelain -- '**/*_gen.go' '**/*_gen_test.go' '**/*_gen.tsx' '**/*_gen.d.ts' '**/*.mapper.go' 'doc/reference/settings_log_components.md' 'doc/development/server-deployment-states.md')" ]; then
+  git status -- '**/*_gen.go' '**/*_gen_test.go' '**/*_gen.tsx' '**/*_gen.d.ts' '**/*.mapper.go' 'doc/reference/settings_log_components.md' 'doc/development/server-deployment-states.md'
   exit 1
 fi
 
